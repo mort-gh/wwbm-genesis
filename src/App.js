2c48cd742b73e92f657e36dcfd0cb42a8142685c
Roman <gnatyuk.info@gmail.com>
2020-10-08 00:50:48 +0300

    Format the file using the new prettier rules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1>Who wants to be a millionaire?</h1>
-    </div>
-  );
+   return (
+      <div>
+         <h1>Who wants to be a millionaire?</h1>
+      </div>
+   );
 }
 
 export default App;
