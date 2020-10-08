1be18a9166f2faed5800e4dc5f27beffd6390b38
Roman <gnatyuk.info@gmail.com>
2020-10-09 00:52:24 +0300

    Add and export propTypes for the Selection module

---
+++ src/modules/game/types/Selection.types.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+export default {
+   questions: PropTypes.instanceOf(Array).isRequired,
+   question: PropTypes.instanceOf(Object).isRequired,
+   score: PropTypes.number.isRequired,
+   questionNumber: PropTypes.number.isRequired,
+   totalQuestions: PropTypes.number.isRequired,
+   disable: PropTypes.bool.isRequired,
+   loading: PropTypes.bool.isRequired,
+   saveScore: PropTypes.func.isRequired,
+   nextQuestionHandler: PropTypes.func.isRequired,
+   history: PropTypes.shape({
+      push: PropTypes.func.isRequired,
+   }).isRequired,
+};
