647631fb7dfe54ba11928cf83b647fc605c3d92e
Roman <gnatyuk.info@gmail.com>
2020-10-09 00:43:27 +0300

    Connect app modules to router switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+// libraries
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+
+// modules
 import Home from './modules/home/screens/Home';
+import Game from './modules/game/containers/Game.container';
+import GameOver from './modules/gameOver/containers/GameOver.container';
+
+// services
+import route from './routes/routes';
 
 function App() {
    return (
       <Switch>
-         <Route path="/" exact component={Home} />
-         <Redirect to="/" />
+         <Route path={route.Home} exact component={Home} />
+         <Route path={route.Game} component={Game} />
+         <Route path={route.GameOver} component={GameOver} />
+         <Redirect to={route.Home} />
       </Switch>
    );
 }
