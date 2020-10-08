2a4d031169654df0f2fcc556d1e27c9931f34836
Roman <gnatyuk.info@gmail.com>
2020-10-09 00:46:48 +0300

    Add button link to start new game

--- src/modules/home/screens/Home.js
+++ src/modules/home/screens/Home.js
@@ -1,13 +1,20 @@
+// libraries
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+// services
+import route from '../../../routes/routes';
 
 const Home = () => {
    return (
       <div className="home">
          <img className="home__img" src="" alt="" />
          <h1 className="home__title">Who wants to be a millionaire?</h1>
-         <button className="btn home__btn" type="button">
-            Start
-         </button>
+         <Link to={route.Game}>
+            <button className="btn home__btn" type="button">
+               Start
+            </button>
+         </Link>
       </div>
    );
 };
