8df06be61ffd8cf4c19c65015e6d1caa6f02c153
Roman <gnatyuk.info@gmail.com>
2020-10-08 14:39:14 +0300

    Initialize the Home module

---
+++ src/modules/home/screens/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+   return (
+      <div className="home">
+         <img className="home__img" src="" alt="" />
+         <h1 className="home__title">Who wants to be a millionaire?</h1>
+         <button className="btn home__btn" type="button">
+            Start
+         </button>
+      </div>
+   );
+};
+
+export default Home;
