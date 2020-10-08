bf7504596f5d96e5b06c2e08a1795f3c53526ef5
Roman <gnatyuk.info@gmail.com>
2020-10-09 00:47:24 +0300

    Remove home reducer and add game reducer

--- src/redux_setup/rootReducer.js
+++ src/redux_setup/rootReducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 
 // reducers
-import home from '../modules/home/redux/homeReducer';
+import game from '../modules/game/redux/Game.Reducer';
 
-const rootReducer = combineReducers({ home });
+const rootReducer = combineReducers({ game });
 
 export default rootReducer;
