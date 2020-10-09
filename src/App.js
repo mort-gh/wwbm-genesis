83543d9f0374cd58d56b7fb9a8e7f63592819416
Roman <gnatyuk.info@gmail.com>
2020-10-09 13:32:37 +0300

    Connect Panel module to switch router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+// services
+import route from './routes/routes';
+
 // modules
 import Home from './modules/home/screens/Home';
 import Game from './modules/game/containers/Game.container';
 import GameOver from './modules/gameOver/containers/GameOver.container';
-
-// services
-import route from './routes/routes';
+import Panel from './modules/panel/screens/Panel';
 
 function App() {
    return (
@@ -16,6 +17,7 @@ function App() {
          <Route path={route.Home} exact component={Home} />
          <Route path={route.Game} component={Game} />
          <Route path={route.GameOver} component={GameOver} />
+         <Route path={route.Panel} component={Panel} />
          <Redirect to={route.Home} />
       </Switch>
    );
