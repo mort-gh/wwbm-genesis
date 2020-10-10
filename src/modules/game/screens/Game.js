32e14e37a987df65a2ac028f10385574a0d32342
Roman <gnatyuk.info@gmail.com>
2020-10-10 17:22:54 +0300

    Add html markup for the burger-menu

--- src/modules/game/screens/Game.js
+++ src/modules/game/screens/Game.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+
 // libraries
 import React from 'react';
 
@@ -8,13 +10,22 @@ import GamePropTypes from '../types/Game.types';
 import Score from '../containers/Score.container';
 import Selection from '../containers/Selection.container';
 
-const Game = ({ question }) => {
+const Game = () => {
+   const burgerMenu = (children) => {
+      return (
+         <div className="burger-menu">
+            <input id="menu__toggle" type="checkbox" />
+            <label className="menu__btn" htmlFor="menu__toggle">
+               <span> </span>
+            </label>
+            {children}
+         </div>
+      );
+   };
    return (
       <div className="game">
-         <p className="question__text">{question.text}</p>
-
          <Selection />
-         <Score />
+         {burgerMenu(<Score />)}
       </div>
    );
 };
