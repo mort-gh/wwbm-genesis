44fd79c93546b40d4e55e42fffe01a4dd550950a
Roman <gnatyuk.info@gmail.com>
2020-10-12 19:46:22 +0300

    Add getNextQuestionData action to dispatch props

--- src/modules/game/containers/Selection.container.js
+++ src/modules/game/containers/Selection.container.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 
 // actions
-import { saveScore, nextQuestionHandler, disableButtons } from '../redux/Game.actions';
+import { saveScore, nextQuestionHandler, disableButtons, getNextQuestionData } from '../redux/Game.actions';
 
 // module
 import Selection from '../screens/Selecton';
@@ -15,6 +15,7 @@ const mapDispatchToProps = (dispatch) => ({
    saveScore: (value) => dispatch(saveScore(value)),
    disableButtons: (bool) => dispatch(disableButtons(bool)),
    nextQuestionHandler: (num, score) => dispatch(nextQuestionHandler(num, score)),
+   getNextQuestionData: (num) => dispatch(getNextQuestionData(num)),
 });
 
 export default compose(withRouter, connect(mapStateToProps, mapDispatchToProps))(Selection);
