f145e7c64ffb0bb19ce3ef5741fa48d2f8d3bc60
Roman <gnatyuk.info@gmail.com>
2020-10-12 20:15:17 +0300

    Shuffle the answer options in the initialization state

--- src/modules/game/redux/Game.store.js
+++ src/modules/game/redux/Game.store.js
@@ -1,8 +1,15 @@
 import questions from '../../../db/questions.json';
+import { toShuffle } from '../../../shared/utils';
+
+// Get the first question from the list:
+const initialQuestion = questions[0];
+// Add random answers before rendering the first question:
+const shuffledOptions = toShuffle(initialQuestion.options);
+initialQuestion.options = shuffledOptions;
 
 export default {
    questions,
-   question: questions[0],
+   question: initialQuestion,
    questionNumber: 1,
    score: 0,
    totalQuestions: questions.length,
